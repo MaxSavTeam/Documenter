0a1a3b79db51177ecb63a56081ac771a4df76666
Max Savitsky <maxsavitsky2015@gmail.com>
2020-11-17 19:14:15 +0300

    ExceptionHandler integrated

---
+++ app/src/main/java/com/maxsavitsky/documenter/AfterExceptionActivity.java
@@ -0,0 +1,86 @@
+package com.maxsavitsky.documenter;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+
+import androidx.appcompat.app.AlertDialog;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class AfterExceptionActivity extends ThemeActivity {
+	private String path;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.activity_errhandler );
+
+		path = getIntent().getStringExtra( "path" );
+
+		findViewById( R.id.btnErrSendReport ).setOnClickListener( v->MainActivity.getInstance().sendLog(path) );
+		findViewById( R.id.btnErrViewReport ).setOnClickListener( v->{
+			FileInputStream inputStream = null;
+			String mes = "";
+			try{
+				inputStream = new FileInputStream( new File(path) );
+				byte[] buffer = new byte[1024];
+				int len;
+				while((len = inputStream.read(buffer)) != -1){
+					if(len < 1024)
+						buffer = Arrays.copyOf(buffer, len);
+
+					mes = String.format( "%s%s", mes, new String(buffer, StandardCharsets.UTF_8 ) );
+				}
+			}catch (Exception e){
+				try {
+					if(inputStream != null)
+						inputStream.close();
+				}catch (IOException io){
+					//ignore
+				}
+				return;
+			}
+			AlertDialog.Builder builder = new AlertDialog.Builder( this )
+					.setCancelable( false )
+					.setNeutralButton( "OK", new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							dialog.cancel();
+						}
+					} ).setMessage( mes );
+			builder.create().show();
+		} );
+
+		findViewById( R.id.btnErrRestartApp ).setOnClickListener( new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				restartApp();
+			}
+		} );
+		AlertDialog.Builder builder = new AlertDialog.Builder( this )
+				.setTitle( R.string.please )
+				.setMessage( Html.fromHtml( getResources().getString( R.string.send_report_dialog_mes ) ) )
+				.setCancelable( false ).setPositiveButton( "OK", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.cancel();
+						MainActivity.getInstance().sendLog(path);
+					}
+				} );
+
+		builder.create().show();
+	}
+
+	private void restartApp() {
+		Intent intent = new Intent( this, MainActivity.class );
+		startActivity( intent );
+		this.finish();
+	}
+}
--- app/src/main/java/com/maxsavitsky/documenter/MainActivity.java
+++ app/src/main/java/com/maxsavitsky/documenter/MainActivity.java
@@ -1,21 +1,18 @@
 package com.maxsavitsky.documenter;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.StrictMode;
-import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.Toolbar;
 
 import com.maxsavitsky.documenter.backup.AutonomousCloudBackupper;
@@ -24,20 +21,18 @@ import com.maxsavitsky.documenter.codes.Results;
 import com.maxsavitsky.documenter.data.MainData;
 import com.maxsavitsky.documenter.ui.CategoryList;
 import com.maxsavitsky.documenter.utils.Utils;
+import com.maxsavitsky.exceptionhandler.ExceptionHandler;
 
 import org.xml.sax.SAXException;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 
 public class MainActivity extends ThemeActivity {
 	private String path;
 	public static final String TAG = "Documenter";
-	private MyExceptionHandler mExceptionHandler;
+	private ExceptionHandler mExceptionHandler;
 
 	private static MainActivity instance;
 
@@ -45,19 +40,17 @@ public class MainActivity extends ThemeActivity {
 		return instance;
 	}
 
-	private boolean isAfterCrash = false;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		mExceptionHandler = new MyExceptionHandler( this );
+		mExceptionHandler = new ExceptionHandler( this, AfterExceptionActivity.class );
 
-		Utils.setContext(this);
+		Utils.setContext( this );
 		SharedPreferences sharedPreferences = getSharedPreferences( Utils.APP_PREFERENCES, Context.MODE_PRIVATE );
 		Utils.setDefaultSharedPreferences( sharedPreferences );
 
-		super.onCreate(savedInstanceState);
+		super.onCreate( savedInstanceState );
 		instance = this;
-		setContentView(R.layout.layout_onstartup);
+		setContentView( R.layout.layout_onstartup );
 		Thread.setDefaultUncaughtExceptionHandler( new Thread.UncaughtExceptionHandler() {
 			@Override
 			public void uncaughtException(@NonNull Thread t, @NonNull Throwable e) {
@@ -67,121 +60,122 @@ public class MainActivity extends ThemeActivity {
 		try {
 			Toolbar toolbar = findViewById( R.id.toolbar );
 			setSupportActionBar( toolbar );
-		}catch (Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		deleteInstalledApks();
 
-		Intent startIntent = getIntent();
-		if(startIntent.getBooleanExtra( "crash", false )){
-			path = startIntent.getStringExtra( "path" );
-			isAfterCrash = true;
-			setContentView( R.layout.activity_errhandler );
-			preparationAfterCrash();
-		}else{
-			final AutonomousCloudBackupper backupper = new AutonomousCloudBackupper( this );
-			new Thread( new Runnable() {
-				@Override
-				public void run() {
-					backupper.stateChanged();
-				}
-			}, "AutoBackupper" ).start();
-		}
-
+		final AutonomousCloudBackupper backupper = new AutonomousCloudBackupper( this );
+		new Thread( backupper::stateChanged, "AutoBackupper" ).start();
 	}
 
 	@Override
 	protected void onPostCreate(@Nullable Bundle savedInstanceState) {
 		super.onPostCreate( savedInstanceState );
-		if(!isAfterCrash) {
-			new Thread( new Runnable() {
-				@Override
-				public void run() {
-					try {
-						initialize();
-						Log.i(TAG, "Initialized");
-					} catch (final Exception e) {
-						runOnUiThread( new Runnable() {
-							@Override
-							public void run() {
-								Utils.getErrorDialog( e, MainActivity.this ).show();
-							}
-						} );
-						return;
+		new Thread( ()->{
+			try {
+				initialize();
+				Log.i( TAG, "Initialized" );
+			} catch (final Exception e) {
+				runOnUiThread( new Runnable() {
+					@Override
+					public void run() {
+						Utils.getErrorDialog( e, MainActivity.this ).show();
 					}
-					try {
-						Thread.sleep( 1000 );
-						viewCategoryList( null );
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			} ).start();
+				} );
+				return;
+			}
+			try {
+				Thread.sleep( 1000 );
+				viewCategoryList( null );
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		} ).start();
+	}
+
+	public void clearAllTraces(View v) {
+		File file = new File( Utils.getExternalStoragePath().getPath() + "/stacktraces/" );
+		if ( file.exists() ) {
+			int count = 0;
+			File[] files = file.listFiles();
+			for (File subFile : files) {
+				subFile.delete();
+				count++;
+			}
+			file.delete();
+			Toast.makeText( this, "Deleted " + count + " files", Toast.LENGTH_SHORT ).show();
 		}
 	}
 
-	private void preparationAfterCrash(){
-		findViewById( R.id.btnErrSendReport ).setOnClickListener( new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				sendLog(path);
+	private void deleteInstalledApks() {
+		File file = new File( Environment.getExternalStorageDirectory().getPath() + "/.documenter" );
+		if ( file.exists() ) {
+			File[] files = file.listFiles();
+			for (File subFile : files) {
+				subFile.delete();
 			}
-		} );
-		findViewById( R.id.btnErrViewReport ).setOnClickListener( new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				FileInputStream inputStream = null;
-				String mes = "";
-				try{
-					inputStream = new FileInputStream( new File(path) );
-					byte[] buffer = new byte[1024];
-					int len;
-					while((len = inputStream.read(buffer)) != -1){
-						if(len < 1024)
-							buffer = Arrays.copyOf(buffer, len);
 
-						mes = String.format( "%s%s", mes, new String(buffer, StandardCharsets.UTF_8 ) );
-					}
-				}catch (Exception e){
-					try {
-						if(inputStream != null)
-							inputStream.close();
-					}catch (IOException io){
-						//ignore
-					}
-					return;
-				}
-				AlertDialog.Builder builder = new AlertDialog.Builder( MainActivity.this )
-						.setCancelable( false )
-						.setNeutralButton( "OK", new DialogInterface.OnClickListener() {
-							@Override
-							public void onClick(DialogInterface dialog, int which) {
-								dialog.cancel();
-							}
-						} ).setMessage( mes );
-				builder.create().show();
-			}
-		} );
+			file.delete();
+		}
+	}
 
-		findViewById( R.id.btnErrRestartApp ).setOnClickListener( new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				restartApp();
-			}
-		} );
-		AlertDialog.Builder builder = new AlertDialog.Builder( this )
-				.setTitle( R.string.please )
-				.setMessage( Html.fromHtml( getResources().getString( R.string.send_report_dialog_mes ) ) )
-				.setCancelable( false ).setPositiveButton( "OK", new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						dialog.cancel();
-						sendLog(path);
-					}
-				} );
+	private long getUsedMemory() {
+		Runtime runtime = Runtime.getRuntime();
+		return runtime.totalMemory() - runtime.freeMemory();
+	}
 
-		builder.create().show();
+	private String getMemoryInfo() {
+		Runtime runtime = Runtime.getRuntime();
+		final double MB = 1024 * 1024;
+		return "Total memory: " + ( runtime.totalMemory() / MB ) + " MB\n" +
+				"Free memory: " + ( runtime.freeMemory() / MB ) + " MB\n" +
+				"Used memory: " + ( getUsedMemory() / MB ) + " MB";
+	}
+
+	public void getUsedMemory(View v) {
+		Toast.makeText( this, getMemoryInfo(), Toast.LENGTH_LONG ).show();
+	}
+
+	private void initialize() throws IOException, SAXException {
+		MainData.readAllCategories();
+		MainData.readAllDocuments();
+		MainData.readAllEntries();
+	}
+
+	public void reinitialize(View v) {
+		MainData.clearAll();
+
+		try {
+			initialize();
+
+			Toast.makeText( this, "Successful\n\n" + getMemoryInfo(), Toast.LENGTH_SHORT ).show();
+		} catch (Exception e) {
+			Utils.getErrorDialog( e, this ).show();
+		}
+	}
+
+	public void clearRam(View v) {
+		MainData.clearAll();
+		System.gc();
+		int MB = 1024 * 1024;
+		Toast.makeText( this, "Memory cleared\n\n" + getMemoryInfo(), Toast.LENGTH_SHORT ).show();
+	}
+
+	public void viewCategoryList(View v) {
+		Intent intent = new Intent( this, CategoryList.class );
+		startActivityForResult( intent, Requests.CATEGORY_LIST );
+	}
+
+	public void makeError(View v) {
+		BigDecimal a = BigDecimal.ONE.divide( BigDecimal.valueOf( 3 ) );
+	}
+
+	private void restartApp() {
+		Intent intent = new Intent( this, MainActivity.class );
+		startActivity( intent );
+		this.finish();
 	}
 
 	public void sendLog(String logPath) {
@@ -201,100 +195,18 @@ public class MainActivity extends ThemeActivity {
 		}
 	}
 
-	public void clearAllTraces(View v){
-		File file = new File( Utils.getExternalStoragePath().getPath() + "/stacktraces/" );
-		if(file.exists()){
-			int count = 0;
-			File[] files = file.listFiles();
-			for(File subFile : files){
-				subFile.delete();
-				count++;
-			}
-			file.delete();
-			Toast.makeText( this, "Deleted " + count + " files", Toast.LENGTH_SHORT ).show();
-		}
-	}
-
-	private void deleteInstalledApks(){
-		File file = new File( Environment.getExternalStorageDirectory().getPath() + "/.documenter" );
-		if(file.exists()){
-			File[] files = file.listFiles();
-			for(File subFile : files){
-				subFile.delete();
-			}
-
-			file.delete();
-		}
-	}
-
-	private long getUsedMemory(){
-		Runtime runtime =  Runtime.getRuntime();
-		return runtime.totalMemory() - runtime.freeMemory();
-	}
-
-	private String getMemoryInfo(){
-		Runtime runtime = Runtime.getRuntime();
-		final double MB = 1024 * 1024;
-		return "Total memory: " + (runtime.totalMemory() / MB) + " MB\n" +
-				"Free memory: " + (runtime.freeMemory() / MB) + " MB\n" +
-				"Used memory: " + (getUsedMemory() / MB) + " MB";
-	}
-
-	public void getUsedMemory(View v){
-		Toast.makeText( this, getMemoryInfo(), Toast.LENGTH_LONG ).show();
-	}
-
-	private void initialize() throws IOException, SAXException {
-		MainData.readAllCategories();
-		MainData.readAllDocuments();
-		MainData.readAllEntries();
-	}
-
-	public void reinitialize(View v){
-		MainData.clearAll();
-
-		try{
-			initialize();
-
-			Toast.makeText( this, "Successful\n\n" + getMemoryInfo(), Toast.LENGTH_SHORT ).show();
-		}catch (Exception e){
-			Utils.getErrorDialog( e, this ).show();
-		}
-	}
-
-	public void clearRam(View v){
-		MainData.clearAll();
-		System.gc();
-		int MB = 1024 * 1024;
-		Toast.makeText( this, "Memory cleared\n\n" + getMemoryInfo(), Toast.LENGTH_SHORT ).show();
-	}
-
-	public void viewCategoryList(View v){
-		Intent intent = new Intent(this, CategoryList.class);
-		startActivityForResult( intent, Requests.CATEGORY_LIST );
-	}
-
-	public void makeError(View v){
-		BigDecimal a = BigDecimal.ONE.divide( BigDecimal.valueOf( 3 ) );
-	}
-
-	private void restartApp() {
-		Intent intent = new Intent( this, MainActivity.class );
-		startActivity( intent );
-		this.finish();
-	}
-
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-		if(requestCode == Requests.CATEGORY_LIST ){
-			if(resultCode == Results.EXIT ){
+		if ( requestCode == Requests.CATEGORY_LIST ) {
+			if ( resultCode == Results.EXIT ) {
 				finishAndRemoveTask();
 			}
-			if(resultCode == Results.RESTART_ACTIVITY ){
+			if ( resultCode == Results.RESTART_ACTIVITY ) {
 				viewCategoryList( null );
 			}
-			if(resultCode != Results.LOOK_STARTUP)
+			if ( resultCode != Results.LOOK_STARTUP ) {
 				setContentView( R.layout.activity_main );
+			}
 		}
 		if ( resultCode == Results.RESTART_APP ) {
 			restartApp();
