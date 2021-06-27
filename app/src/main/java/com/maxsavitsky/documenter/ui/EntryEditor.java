19e3fa8dd208b137338ff5d6702f1c733ff91404
Max Savitsky <maxsavitsky2015@gmail.com>
2021-06-27 16:29:27 +0300

    fixed images picking

--- app/src/main/java/com/maxsavitsky/documenter/ui/EntryEditor.java
+++ app/src/main/java/com/maxsavitsky/documenter/ui/EntryEditor.java
@@ -1,6 +1,5 @@
 package com.maxsavitsky.documenter.ui;
 
-import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -14,7 +13,6 @@ import android.graphics.PorterDuffColorFilter;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.Editable;
@@ -120,48 +118,40 @@ public class EntryEditor extends ThemeActivity {
 	private final String TAG = App.TAG + " EntryEditor";
 	private Group mParentGroup;
 
-	private final ActivityResultLauncher<Intent> mPickImageLauncher = registerForActivityResult(
-			new ActivityResultContracts.StartActivityForResult(),
-			result -> {
-				if ( result.getResultCode() == Activity.RESULT_OK ) {
-					Intent data = result.getData();
-					if ( data == null ) {
-						Toast.makeText( this, "Data is null", Toast.LENGTH_SHORT ).show();
+	private final ActivityResultLauncher<String> mPickImageLauncher = registerForActivityResult(
+			new ActivityResultContracts.GetContent(),
+			uri->{
+				if ( uri == null ) {
+					Toast.makeText( this, "Some error occurred", Toast.LENGTH_SHORT ).show();
+					return;
+				}
+				try (InputStream in = getContentResolver().openInputStream( uri )) {
+					String type = getContentResolver().getType( uri );
+					if ( in == null || type == null ) {
+						Toast.makeText( this, "Some problems with reading", Toast.LENGTH_SHORT ).show();
 						return;
 					}
-					Uri uri = data.getData();
-					if ( uri == null ) {
-						Toast.makeText( this, "Some error occurred", Toast.LENGTH_SHORT ).show();
-						return;
+					File file = Utils.getEntryImagesMediaFolder( mEntry.getId() );
+					String format = MimeTypeMap.getSingleton().getExtensionFromMimeType( getContentResolver().getType( uri ) );
+					file = new File( file, Utils.generateUniqueId() + "." + format );
+					if ( mEntry.getId().equals( "temp_entry" ) ) {
+						mMediaToMove.add( file );
 					}
-					try (InputStream in = getContentResolver().openInputStream( uri )) {
-						String type = getContentResolver().getType( uri );
-						if ( in == null || type == null ) {
-							Toast.makeText( this, "Some problems with reading", Toast.LENGTH_SHORT ).show();
-							return;
-						}
-						File file = Utils.getEntryImagesMediaFolder( mEntry.getId() );
-						String format = MimeTypeMap.getSingleton().getExtensionFromMimeType( getContentResolver().getType( uri ) );
-						file = new File( file, Utils.generateUniqueId() + "." + format );
-						if ( mEntry.getId().equals( "temp_entry" ) ) {
-							mMediaToMove.add( file );
-						}
-						FileOutputStream fos = new FileOutputStream( file );
-						int len;
-						byte[] buffer = new byte[ 1024 ];
-						while ( ( len = in.read( buffer ) ) != -1 ) {
-							fos.write( buffer, 0, len );
-						}
-						fos.close();
-						in.close();
+					FileOutputStream fos = new FileOutputStream( file );
+					int len;
+					byte[] buffer = new byte[ 1024 ];
+					while ( ( len = in.read( buffer ) ) != -1 ) {
+						fos.write( buffer, 0, len );
+					}
+					fos.close();
+					in.close();
 
-						setImageAtSelBounds( file );
-					} catch (IOException | NullPointerException e) {
-						e.printStackTrace();
-						Utils.getErrorDialog( e, this ).show();
-						Toast.makeText( this, "Picture could be damaged.\n" +
-								"We recommend deleting and re-adding.", Toast.LENGTH_LONG ).show();
-					}
+					setImageAtSelBounds( file );
+				} catch (Exception e) {
+					e.printStackTrace();
+					Utils.getErrorDialog( e, this ).show();
+					Toast.makeText( this, "Picture could be damaged.\n" +
+							"We recommend deleting and re-adding.", Toast.LENGTH_LONG ).show();
 				}
 			}
 	);
@@ -680,9 +670,7 @@ public class EntryEditor extends ThemeActivity {
 	}
 
 	public void pickImages(View v) {
-		Intent picker = new Intent( Intent.ACTION_GET_CONTENT );
-		picker.setType( "image/*" );
-		mPickImageLauncher.launch( picker );
+		mPickImageLauncher.launch( "image/*" );
 	}
 
 	private void setImageAtSelBounds(File file) {
